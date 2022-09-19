92c15f16b7a05e3b66ff7ba1894cf4177f4fb52c
Jun, Hongyoung <royqwe16@gmail.com>
2022-09-19 17:01:11 +0900

    Hello 클래스 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -1,8 +1,9 @@
 package git_test2;
 
 public class Main {
-    public static void main(String[] args) {
-        new Hello().say();
-    }
 
+    public static void main(String[] args) {
+        Hello h = new Hello("hello world");
+        h.say();
+    }
 }
