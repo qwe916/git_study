830814c57fbe247dc4b4572674efb82a7041850d
Jun, Hongyoung <royqwe16@gmail.com>
2022-09-26 17:09:52 +0900

    Main.java 수정 #4

--- src/e201814100/Main.java
+++ src/e201814100/Main.java
@@ -1,8 +1,15 @@
 package e201814100;
 
 public class Main {
-	public static void main(String[] args) {
-		Hello hello = new Hello();
-		hello.say("hello world");
-	}
+
+    static void say() {
+        System.out.println("main");
+    }
+    
+    public static void main(String[] args) {
+        say();
+        Hello hello = new Hello();
+        hello.say("hello world");
+    }
 }
+
